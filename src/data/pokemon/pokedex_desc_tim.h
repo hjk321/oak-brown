9b70fc275227744608c6b70f2ea349ee8fb8f3ad
hjk321 <anderdude@outlook.com>
2021-03-03 21:53:51 -0500

    Starter Pokédex Entries

--- src/data/pokemon/pokedex_desc_tim.h
+++ src/data/pokemon/pokedex_desc_tim.h
@@ -1,4 +1,13 @@
 const struct PokedexDesc gTimNotes[NATIONAL_DEX_COUNT + 1][DEX_DESC_PER_MON] =
 {
     [NATIONAL_DEX_NONE] = {{0, gTimNotesDummy}},
+    [NATIONAL_DEX_BULBASAUR] = {{0, gTimNotesBulbasaur}},
+    [NATIONAL_DEX_IVYSAUR] = {{0, gTimNotesIvysaur}},
+    [NATIONAL_DEX_VENUSAUR] = {{0, gTimNotesVenusaur}},
+    [NATIONAL_DEX_CHARMANDER] = {{0, gTimNotesCharmander}},
+    [NATIONAL_DEX_CHARMELEON] = {{0, gTimNotesCharmeleon}},
+    [NATIONAL_DEX_CHARIZARD] = {{0, gTimNotesCharizard}},
+    [NATIONAL_DEX_SQUIRTLE] = {{0, gTimNotesSquirtle}},
+    [NATIONAL_DEX_WARTORTLE] = {{0, gTimNotesWartortle}},
+    [NATIONAL_DEX_BLASTOISE] = {{0, gTimNotesBlastoise}},
 };
--- src/data/pokemon/pokedex_text_tim.h
+++ src/data/pokemon/pokedex_text_tim.h
@@ -1,2 +1,73 @@
 const u8 gTimNotesDummy[] = _(
     "I don't know anything about this Pokémon.");
+
+const u8 gTimNotesBulbasaur[] = _(
+    "Bulbasaur have been observed taking care\n"
+    "of plants in the wild, so they make a\n"
+    "good companion for gardeners and grounds\n"
+    "keepers."
+);
+
+const u8 gTimNotesIvysaur[] = _(
+    "The larger variety of berries you feed\n"
+    "it, the more vibrant and colorful its\n"
+    "flower will be at full bloom. Its petals\n"
+    "wilt naturally over time and can be used\n"
+    "in herbal teas."
+);
+
+const u8 gTimNotesVenusaur[] = _(
+    "A common misconception about Venusaur is\n"
+    "that it needs only sunlight to survive.\n"
+    "While it can live for a few months this\n"
+    "way, its health depends on nutrients from\n"
+    "berries in order to stay strong."
+);
+
+const u8 gTimNotesCharmander[] = _(
+    "It became a very popular starter choice\n"
+    "after a child prodigy named Red defeated\n"
+    "the Indigo league with a Charizard. Many\n"
+    "younger trainers have a hard time raising\n"
+    "it, however."
+);
+const u8 gTimNotesCharmeleon[] = _(
+    "While technically omnivorous, it craves\n"
+    "meat much more aggressively than\n"
+    "Charmander, so trainers should allow it\n"
+    "to hunt smaller Pokémon while training."
+);
+
+const u8 gTimNotesCharizard[] = _(
+    "Charizard is considered a symbol of grace\n"
+    "and elegance in some regions. However,\n"
+    "some dragon tamers dislike it, since it\n"
+    "is technically not a dragon type, and is\n"
+    "relatively tame compared to most dragons."
+);
+
+const u8 gTimNotesSquirtle[] = _(
+    "It sometimes employs a unique fighting\n"
+    "style, drawin itself into a shell and\n"
+    "spinning toward opponents at high speed.\n"
+    "This inspired a company in Kanto to make\n"
+    "a game with Squirtle as enemies."
+);
+
+const u8 gTimNotesWartortle[] = _(
+    "A rumor has circulated that Wartortle can\n"
+    "live for thousands of years. While this\n"
+    "is false, it can indeed live for over two\n"
+    "centuries, as long as it does not evolve\n"
+    "into Blastoise."
+);
+
+const u8 gTimNotesBlastoise[] = _(
+    "The Squirtle line is often portrayed\n"
+    "incorrectly in media as eating reeds and\n"
+    "other aquatic plant matter. In actuality,\n"
+    "they are piscivores. Blastoise eats half\n"
+    "its body weight in fish every day."
+);
+
+//  "-----------------------------------------"
