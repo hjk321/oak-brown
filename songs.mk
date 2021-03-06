c5cd975bc1b4ae248c11c20ebcd4c26fb8482f5c
hjk321 <anderdude@outlook.com>
2021-03-06 14:16:50 -0500

    Rename town theme to correct town

--- songs.mk
+++ songs.mk
@@ -282,7 +282,7 @@ $(MID_SUBDIR)/mus_victory_trainer.s: %.s: %.mid
 $(MID_SUBDIR)/mus_victory_wild.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G170 -V090
 
-$(MID_SUBDIR)/mus_seashore.s: %.s: %.mid
+$(MID_SUBDIR)/mus_gravel.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G159 -V100
 
 $(MID_SUBDIR)/ph_choice_blend.s: %.s: %.mid
