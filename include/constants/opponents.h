53c1cd8620c1ef4d50f18ccf7529505640579e99
hjk321 <37224753+hjk321@users.noreply.github.com>
2021-08-20 19:39:46 -0600

    Merson Cave Events

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -8,12 +8,20 @@
 #define TRAINER_MURA_CHARMANDER_1                   2
 #define TRAINER_MURA_SQUIRTLE_1                     3
 #define TRAINER_YOUNGSTER_HAYDEN                    4
+#define TRAINER_YOUNGSTER_BENNY                     5
+#define TRAINER_LASS_ABIGAIL                        6
+#define TRAINER_BUG_CATCHER_FRED                    7
+#define TRAINER_SUPER_NERD_JONATHAN                 8
+#define TRAINER_HIKER_BRIAN                         9
+#define TRAINER_SUPER_NERD_JARUS                    10
+#define TRAINER_LASS_NATALIE                        11
+#define TRAINER_BUG_CATCHER_GEORGE                  12
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is 
 //       only space for 25 additional trainers before trainer flag space overflows.
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define NUM_TRAINERS                             4
+#define NUM_TRAINERS                             12
 #define MAX_TRAINERS_COUNT                       768
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_H
--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -44,3 +44,118 @@ static const struct TrainerMon sTrainerMons_YoungsterHayden[] = {
         .ball = ITEM_POKE_BALL,
     },
 };
+
+static const struct TrainerMon sTrainerMons_YoungsterBenny[] = {
+    {
+        .lvl = 3,
+        .species = SPECIES_RATTATA,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 3,
+        .species = SPECIES_PATRAT,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 3,
+        .species = SPECIES_ZUBAT,
+        .ball = ITEM_POKE_BALL,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_LassAbigail[] = {
+    {
+        .lvl = 5,
+        .species = SPECIES_PIKACHU,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 5,
+        .species = SPECIES_CLEFAIRY,
+        .ball = ITEM_LUXURY_BALL,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_BugCatcherFred[] = {
+    {
+        .lvl = 6,
+        .species = SPECIES_CATERPIE,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 8,
+        .species = SPECIES_YANMA,
+        .ball = ITEM_NET_BALL,
+    },
+    {
+        .lvl = 6,
+        .species = SPECIES_WEEDLE,
+        .ball = ITEM_POKE_BALL,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_SuperNerdJonathan[] = {
+    {
+        .lvl = 9,
+        .species = SPECIES_PORYGON,
+        .ball = ITEM_TIMER_BALL,
+    },
+    {
+        .lvl = 9,
+        .species = SPECIES_ROTOM,
+        .ball = ITEM_POKE_BALL,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_HikerBrian[] = {
+    {
+        .lvl = 7,
+        .species = SPECIES_RATTATA,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 8,
+        .species = SPECIES_SPEAROW,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 8,
+        .species = SPECIES_KLINK,
+        .ball = ITEM_POKE_BALL,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_SuperNerdJarus[] = {
+    {
+        .lvl = 8,
+        .species = SPECIES_GEODUDE,
+        .ball = ITEM_POKE_BALL,
+    },
+    {
+        .lvl = 8,
+        .species = SPECIES_SHIELDON,
+        .ball = ITEM_POKE_BALL,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_LassNatalie[] = {
+    {
+        .lvl = 9,
+        .species = SPECIES_CLEFAIRY,
+        .ball = ITEM_PREMIER_BALL,
+        .heldItem = ITEM_ORAN_BERRY,
+    },
+};
+
+static const struct TrainerMon sTrainerMons_BugCatcherGeorge[] = {
+    {
+        .lvl = 8,
+        .species = SPECIES_WEEDLE,
+        .ball = ITEM_NEST_BALL,
+    },
+    {
+        .lvl = 8,
+        .species = SPECIES_PARAS,
+        .ball = ITEM_NET_BALL,
+    },
+};
