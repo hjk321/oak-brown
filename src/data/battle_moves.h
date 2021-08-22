b3eec7cbd19fff2d6c0335f9797226db53ee80a4
Peter Swanton <pjswan8691@ung.edu>
2021-08-22 10:04:29 -0400

    earth power

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -4982,4 +4982,18 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED/* | FLAG_BALLISTIC*/ /* | FLAG_SHEER_FORCE_BOOST*/,
         .physicality = MOVE_PHYSICALITY_SPECIAL,
     },
+	
+	[MOVE_EARTH_POWER] =
+    {
+        .effect = EFFECT_SPECIAL_DEFENSE_DOWN_HIT,
+        .power = 90,
+        .type = TYPE_GROUND,
+        .accuracy = 100,
+        .pp = 10,
+        .secondaryEffectChance = 10,
+        .target = MOVE_TARGET_SELECTED,
+        .priority = 0,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED/* | FLAG_SHEER_FORCE_BOOST*/,
+        .physicality = MOVE_PHYSICALITY_SPECIAL,
+    },
 };
