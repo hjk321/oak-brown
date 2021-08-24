rayolsen/LibraRed

Document Pokemon Summary Screen, 1

src/strings.c
@@ -134,14 +134,14 @@ const u8 gText_Mail[] = _("MAIL");
 const u8 gText_Take[] = _("TAKE");
 const u8 gText_Store[] = _("STORE");
 const u8 gOtherText_Check[] = _("CHECK");
-const u8 gUnknown_84161EF[] = _("NONE");
+const u8 gText_PokeSum_Item_None[] = _("NONE");
 const u8 gOtherText_Open[] = _("OPEN");
 const u8 gOtherText_Deselect[] = _("DESELECT");
-const u8 gUnknown_8416202[] = _("???");
+const u8 gText_PokeSum_DexNoUnknown[] = _("???");
 const u8 gUnknown_8416206[] = _("?????");
 const u8 gText_Slash[] = _("/");
-const u8 gUnknown_841620E[] = _("-");
-const u8 gUnknown_8416210[] = _("--");
+const u8 gText_PokeSum_OneHyphen[] = _("-");
+const u8 gText_PokeSum_TwoHyphens[] = _("--");
 const u8 gText_ThreeHyphens[] = _("---");
 const u8 gText_SevenHyphens[] = _("-------");
 const u8 gText_MaleSymbol[] = _("♂");
@@ -803,20 +803,20 @@ const u8 gText_RegionMap_AreaDesc_PatternBush[] = _("A lush and bush-like area. 
 const u8 gText_RegionMap_AreaDesc_DottedHole[] = _("A mysterious, just-discovered\nruin from an ancient time.\nIt got its name from the six dots\non its door.");
 const u8 gUnknown_8419782[] = _("{DYNAMIC 0x00} nature.\nMet in a trade.");
 const u8 gUnknown_841979D[] = _("{DYNAMIC 0x00} nature.\nMet in a trade.");
-const u8 gUnknown_84197B8[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84197ED[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_8419822[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_8419841[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_8419860[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_841988A[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84198B4[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84198D5[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84198F6[] = _("{DYNAMIC 0x00}なせいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
-const u8 gUnknown_8419913[] = _("{DYNAMIC 0x00}せいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
-const u8 gUnknown_841992F[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
-const u8 gUnknown_841996D[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
-const u8 gUnknown_84199AB[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter (hatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}).");
-const u8 gUnknown_84199F4[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter シhatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}ス.");
+const u8 gText_PokeSum_FatefulEncounterMet[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_FatefulEncounterMet_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Met[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Met_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_ApparentlyMet[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_ApparentlyMet_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Hatched[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Hatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_JP_ApparentlyHatched[] = _("{DYNAMIC 0x00}なせいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
+const u8 gText_PokeSum_JP_ApparentlyHatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00}せいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
+const u8 gText_PokeSum_FatefulEncounterHatched[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
+const u8 gText_PokeSum_FatefulEncounterHatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
+const u8 gText_PokeSum_ApparentlyFatefulEncounterHatched[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter (hatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}).");
+const u8 gText_PokeSum_ApparentlyFatefulEncounterHatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter (hatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}).");
 const u8 gUnknown_8419A3D[] = _("An odd POKéMON EGG found by the\nDAY-CARE couple.");
 const u8 gUnknown_8419A6E[] = _("An odd POKéMON EGG obtained from\nthe traveling man.");
 const u8 gUnknown_8419AA2[] = _("A wondrously peculiar POKéMON EGG\nobtained at a nice place.");
@@ -826,22 +826,22 @@ const u8 gUnknown_8419B44[] = _("It looks like this\nEGG will take a\nlong time 
 const u8 gUnknown_8419B7B[] = _("What will hatch\nfrom this? It will\ntake some time.");
 const u8 gUnknown_8419BAE[] = _("It occasionally\nmoves. It should\nhatch soon.");
 const u8 gUnknown_8419BDB[] = _("It's making sounds.\nIt's almost ready\nto hatch!");
-const u8 gUnknown_8419C0B[] = _("a trade");
+const u8 gText_PokeSum_ATrade[] = _("a trade");
 const u8 gUnknown_8419C13[] = _("Somewhere");
-const u8 gUnknown_8419C1D[] = _("POKéMON INFO");
-const u8 gUnknown_8419C2A[] = _("POKéMON SKILLS");
-const u8 gUnknown_8419C39[] = _("KNOWN MOVES");
-const u8 gUnknown_8419C45[] = _("No data");
-const u8 gUnknown_8419C4D[] = _("EXP. POINTS");
-const u8 gUnknown_8419C59[] = _("NEXT LV.");
-const u8 gUnknown_8419C62[] = _("{DPAD_RIGHT}PAGE {A_BUTTON}CANCEL");
-const u8 gUnknown_8419C72[] = _("{A_BUTTON}CANCEL");
-const u8 gUnknown_8419C7B[] = _("{DPAD_LEFTRIGHT}PAGE");
-const u8 gUnknown_8419C82[] = _("{DPAD_LEFT}PAGE {A_BUTTON}DETAIL");
-const u8 gUnknown_8419C92[] = _("{DPAD_UPDOWN}PICK {A_BUTTON}SWITCH");
-const u8 gUnknown_8419CA2[] = _("{DPAD_UPDOWN}PICK");
-const u8 gUnknown_8419CA9[] = _("{DPAD_UPDOWN}PICK {A_BUTTON}DELETE");
-const u8 gUnknown_8419CB9[] = _("HM moves can't be\nforgotten now.");
+const u8 gText_PokeSum_PageName_PokemonInfo[] = _("POKéMON INFO");
+const u8 gText_PokeSum_PageName_PokemonSkills[] = _("POKéMON SKILLS");
+const u8 gText_PokeSum_PageName_KnownMoves[] = _("KNOWN MOVES");
+const u8 gText_PokeSum_NoData[] = _("No data");
+const u8 gText_PokeSum_ExpPoints[] = _("EXP. POINTS");
+const u8 gText_PokeSum_NextLv[] = _("NEXT LV.");
+const u8 gText_PokeSum_Controls_PageCancel[] = _("{DPAD_RIGHT}PAGE {A_BUTTON}CANCEL");
+const u8 gText_PokeSum_Controls_Cancel[] = _("{A_BUTTON}CANCEL");
+const u8 gText_PokeSum_Controls_Page[] = _("{DPAD_LEFTRIGHT}PAGE");
+const u8 gText_PokeSum_Controls_PageDetail[] = _("{DPAD_LEFT}PAGE {A_BUTTON}DETAIL");
+const u8 gText_PokeSum_Controls_PickSwitch[] = _("{DPAD_UPDOWN}PICK {A_BUTTON}SWITCH");
+const u8 gText_PokeSum_Controls_Pick[] = _("{DPAD_UPDOWN}PICK");
+const u8 gText_PokeSum_Controls_PickDelete[] = _("{DPAD_UPDOWN}PICK {A_BUTTON}DELETE");
+const u8 gText_PokeSum_HmMovesCantBeForgotten[] = _("HM moves can't be\nforgotten now.");
 const u8 gText_TrainerCardName[] = _("NAME: ");
 const u8 gText_TrainerCardIDNo[] = _("IDNo.");
 const u8 gText_TrainerCardMoney[] = _("MONEY");

hjk321/oak-brown

src/strings.c
@@ -134,14 +134,14 @@ const u8 gText_Mail[] = _("Mail");
 const u8 gText_Take[] = _("Take");
 const u8 gText_Store[] = _("Store");
 const u8 gOtherText_Check[] = _("Check");
-const u8 gUnknown_84161EF[] = _("None");
+const u8 gText_PokeSum_Item_None[] = _("None");
 const u8 gOtherText_Open[] = _("Open");
 const u8 gOtherText_Deselect[] = _("Deselect");
-const u8 gUnknown_8416202[] = _("???");
+const u8 gText_PokeSum_DexNoUnknown[] = _("???");
 const u8 gUnknown_8416206[] = _("?????");
 const u8 gText_Slash[] = _("/");
-const u8 gUnknown_841620E[] = _("-");
-const u8 gUnknown_8416210[] = _("--");
+const u8 gText_PokeSum_OneHyphen[] = _("-");
+const u8 gText_PokeSum_TwoHyphens[] = _("--");
 const u8 gText_ThreeHyphens[] = _("---");
 const u8 gText_SevenHyphens[] = _("-------");
 const u8 gText_MaleSymbol[] = _("♂");
@@ -821,20 +821,20 @@ const u8 gText_RegionMap_AreaDesc_PatternBush[] = _("A lush and bush-like area. 
 const u8 gText_RegionMap_AreaDesc_DottedHole[] = _("A mysterious, just-discovered\nruin from an ancient time.\nIt got its name from the six dots\non its door.");
 const u8 gUnknown_8419782[] = _("{DYNAMIC 0x00} nature.\nMet in a trade.");
 const u8 gUnknown_841979D[] = _("{DYNAMIC 0x00} nature.\nMet in a trade.");
-const u8 gUnknown_84197B8[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84197ED[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_8419822[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_8419841[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_8419860[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_841988A[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84198B4[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84198D5[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
-const u8 gUnknown_84198F6[] = _("{DYNAMIC 0x00}なせいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
-const u8 gUnknown_8419913[] = _("{DYNAMIC 0x00}せいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
-const u8 gUnknown_841992F[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
-const u8 gUnknown_841996D[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
-const u8 gUnknown_84199AB[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter (hatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}).");
-const u8 gUnknown_84199F4[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter シhatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}ス.");
+const u8 gText_PokeSum_FatefulEncounterMet[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_FatefulEncounterMet_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nMet in a fateful encounter when\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Met[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Met_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nMet in {DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_ApparentlyMet[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_ApparentlyMet_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nApparently met in {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Hatched[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_Hatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature.\nHatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}.");
+const u8 gText_PokeSum_JP_ApparentlyHatched[] = _("{DYNAMIC 0x00}なせいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
+const u8 gText_PokeSum_JP_ApparentlyHatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00}せいかく {LV_2}{DYNAMIC 0x01}のとき\n{DYNAMIC 0x02}で かえった ようだ");
+const u8 gText_PokeSum_FatefulEncounterHatched[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
+const u8 gText_PokeSum_FatefulEncounterHatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature. Met in a fateful\nencounter (hatched: {DYNAMIC 0x02}\nat {LV_2} {DYNAMIC 0x01}).");
+const u8 gText_PokeSum_ApparentlyFatefulEncounterHatched[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter (hatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}).");
+const u8 gText_PokeSum_ApparentlyFatefulEncounterHatched_BoldGentleGrammar[] = _("{DYNAMIC 0x00} nature. Apparently met in\na fateful encounter シhatched:\n{DYNAMIC 0x02} at {LV_2} {DYNAMIC 0x01}ス.");
 const u8 gUnknown_8419A3D[] = _("An odd Pokémon Egg found by the\nDay-Care couple.");
 const u8 gUnknown_8419A6E[] = _("An odd Pokémon Egg obtained from\nthe traveling man.");
 const u8 gUnknown_8419AA2[] = _("A wondrously peculiar Pokémon Egg\nobtained at a nice place.");
@@ -844,22 +844,22 @@ const u8 gUnknown_8419B44[] = _("It looks like this\nEgg will take a\nlong time 
 const u8 gUnknown_8419B7B[] = _("What will hatch\nfrom this? It will\ntake some time.");
 const u8 gUnknown_8419BAE[] = _("It occasionally\nmoves. It should\nhatch soon.");
 const u8 gUnknown_8419BDB[] = _("It's making sounds.\nIt's almost ready\nto hatch!");
-const u8 gUnknown_8419C0B[] = _("a trade");
+const u8 gText_PokeSum_ATrade[] = _("a trade");
 const u8 gUnknown_8419C13[] = _("Somewhere");
-const u8 gUnknown_8419C1D[] = _("Pokémon Info");
-const u8 gUnknown_8419C2A[] = _("Pokémon Skills");
-const u8 gUnknown_8419C39[] = _("Known Moves");
-const u8 gUnknown_8419C45[] = _("No data");
-const u8 gUnknown_8419C4D[] = _("Exp. Points");
-const u8 gUnknown_8419C59[] = _("Next Lv.");
-const u8 gUnknown_8419C62[] = _("{DPAD_RIGHT}Page {A_BUTTON}Cancel");
-const u8 gUnknown_8419C72[] = _("{A_BUTTON}Cancel");
-const u8 gUnknown_8419C7B[] = _("{DPAD_LEFTRIGHT}Page");
-const u8 gUnknown_8419C82[] = _("{DPAD_LEFT}Page {A_BUTTON}Detail");
-const u8 gUnknown_8419C92[] = _("{DPAD_UPDOWN}Pick {A_BUTTON}Switch");
-const u8 gUnknown_8419CA2[] = _("{DPAD_UPDOWN}Pick");
-const u8 gUnknown_8419CA9[] = _("{DPAD_UPDOWN}Pick {A_BUTTON}Delete");
-const u8 gUnknown_8419CB9[] = _("HM moves can't be\nforgotten now.");
+const u8 gText_PokeSum_PageName_PokemonInfo[] = _("Pokémon Info");
+const u8 gText_PokeSum_PageName_PokemonSkills[] = _("Pokémon Skills");
+const u8 gText_PokeSum_PageName_KnownMoves[] = _("Known Moves");
+const u8 gText_PokeSum_NoData[] = _("No data");
+const u8 gText_PokeSum_ExpPoints[] = _("Exp. Points");
+const u8 gText_PokeSum_NextLv[] = _("Next Lv.");
+const u8 gText_PokeSum_Controls_PageCancel[] = _("{DPAD_RIGHT}Page {A_BUTTON}CANCEL");
+const u8 gText_PokeSum_Controls_Cancel[] = _("{A_BUTTON}Cancel");
+const u8 gText_PokeSum_Controls_Page[] = _("{DPAD_LEFTRIGHT}Page");
+const u8 gText_PokeSum_Controls_PageDetail[] = _("{DPAD_LEFT}Page {A_BUTTON}Detail");
+const u8 gText_PokeSum_Controls_PickSwitch[] = _("{DPAD_UPDOWN}Pick {A_BUTTON}Switch");
+const u8 gText_PokeSum_Controls_Pick[] = _("{DPAD_UPDOWN}Pick");
+const u8 gText_PokeSum_Controls_PickDelete[] = _("{DPAD_UPDOWN}Pick {A_BUTTON}Delete");
+const u8 gText_PokeSum_HmMovesCantBeForgotten[] = _("HM moves can't be\nforgotten now.");
 const u8 gText_TrainerCardName[] = _("Name: ");
 const u8 gText_TrainerCardIDNo[] = _("IDNo.");
 const u8 gText_TrainerCardMoney[] = _("Money");