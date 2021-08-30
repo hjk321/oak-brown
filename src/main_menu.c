72341d0a6b2f7f44d35bb9ad96df87dcd9400b22
hjk321 <37224753+hjk321@users.noreply.github.com>
2021-08-30 15:28:33 -0600

    Expand Bag Sizes

--- src/main_menu.c
+++ src/main_menu.c
@@ -855,7 +855,7 @@ static void CheckSaveblockStructSizes(void)
     mgba_printf(MGBA_LOG_INFO, "PokemonStorage size (max 44924):");
     mgba_printf(MGBA_LOG_INFO, "%10d", sizeof(struct PokemonStorage));
     
-    if (sizeof(struct SaveBlock2) > 4048 || sizeof(struct SaveBlock1) > 12252
+    if (sizeof(struct SaveBlock2) > 4084 || sizeof(struct SaveBlock1) > 12252
         || sizeof(struct BoxPokemon) > 64 || sizeof(struct PokemonStorage) > 44924)
     mgba_printf(MGBA_LOG_FATAL, "One or more save structs are too big.");
 #endif
